e4e4264d7981321d86098c387accfab6b1950270
jamespnixon <58857800+jamespnixon@users.noreply.github.com>
2020-01-21 21:53:54 -0800

    Week 3 Day 3

---
+++ WAAPI_Commands_Execute/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace WAAPI_Commands_Execute
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            _Main().Wait();
+        }
+
+        static async Task _Main()
+        {
+            AK.Wwise.Waapi.JsonClient client = new AK.Wwise.Waapi.JsonClient();
+
+            await client.Connect();
+
+            client.Disconnected += () =>
+            {
+                System.Console.WriteLine("We lost connection!");
+            };
+
+            await client.Call(
+                ak.wwise.ui.commands.execute, new
+                {
+                    command = "Solo",
+                    
+                },
+                null
+            );
+
+            await client.Close();
+        }
+    }
+}
