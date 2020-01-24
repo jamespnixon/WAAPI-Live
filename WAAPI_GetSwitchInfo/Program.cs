135798f483834ddd1a69f41b692e4d0bc94e8a0d
jamespnixon <58857800+jamespnixon@users.noreply.github.com>
2020-01-23 21:02:02 -0800

    Experimenting with getting selected object

---
+++ WAAPI_GetSwitchInfo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace WAAPI_GetSwitchInfo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            _Main().Wait();
+        }
+
+        static async Task _Main()
+        {
+            AK.Wwise.Waapi.JsonClient client = new AK.Wwise.Waapi.JsonClient();
+
+        await client.Connect();
+
+        client.Disconnected += () =>
+            {
+            System.Console.WriteLine("We lost connection!");
+            };
+            
+            //get selected object (single object selection for now)
+            var result = await client.Call(
+                ak.wwise.ui.getSelectedObjects);  
+
+            Console.WriteLine(result);
+
+            //return object type from selected object
+            
+            //notify user if no switch container is selected
+
+            //get switchgroup assignment from container.  Notify user.
+        }
+    }
+}
